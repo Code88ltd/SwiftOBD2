0edb824a5491ab0af4659e0981634ac06f0bbea9
Dan Lewis <22656464+Danlewis3@users.noreply.github.com>
2026-02-08 13:35:34 +0000

    Update DTCs in mockManager.swift

--- Sources/SwiftOBD2/Communication/mockManager.swift
+++ Sources/SwiftOBD2/Communication/mockManager.swift
@@ -145,7 +145,7 @@ class MOCKComm: CommProtocol {
 
         } else if command == "03" {
             // 03 is a request for DTCs
-            let dtcs = ["P0135", "P0141"]
+            let dtcs = ["P0104", "U0207"]
             var response = ""
             // convert to hex
             for dtc in dtcs {
